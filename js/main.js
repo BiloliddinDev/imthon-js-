b2f99bc023ae525d19c232c75632cd805a6e37bc
bilol <bilol09876@gmail.com>
2022-08-31 20:44:30 +0500

    afsweadfw

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let arr = ["I", "study", "JavaSkript", "right", "now"];
+
+let newarr = arr.splice(2, 1);
+console.log(arr);
+
+// function juftlig (son) {
+//   if (son <= 0) {
+//     son =
+//     console.log("Juft son")
+//   } else {
+//     "toq son"
+//   }
+//   return
+// }
+
+// console.log(juftlig(5));
